eab60f520cdc66e1e14c86a1f7ca605ebd893d69
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-09-20 23:27:52 +0000

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uppercase(str):
+    j = len(str)
+    i = 0
+
+    while (i < j):
+        if (ord(str[i]) >= 97) and (ord(str[i]) <= 122):
+            shit = ord(str[i])
+            mess = shit - 32
+            piss = chr(mess)
+            print("{}".format(piss), end="")
+        else:
+            print("{}".format(str[i]), end="")
+        i += 1
+    print(" ")
