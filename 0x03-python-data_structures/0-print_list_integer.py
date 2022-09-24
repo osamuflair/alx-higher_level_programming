ab4f36e999917b2dda571ca7866a1462f1fe9353
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-09-24 06:51:41 +0000

    Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        print("An empty list")
     for i in range(len(my_list)):
         print("{}".format(my_list[i]))
