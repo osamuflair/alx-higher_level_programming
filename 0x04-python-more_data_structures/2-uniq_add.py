16ee65d486c86a08cb8f67505143b3554ec94dc8
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-09-28 11:18:19 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import functools
 def uniq_add(my_list=[]):
     mylist = set(my_list)
     mylis = list(mylist)
     if mylis is None:
         return 0
-    return functools.reduce(lambda x,y: x+y, mylis)
+    y = 0
+    for i in mylis:
+        y += i
+    return y
