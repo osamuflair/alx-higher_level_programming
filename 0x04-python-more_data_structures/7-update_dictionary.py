aae1e7224e88e1016854214b4f9574c91c93124a
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-09-29 07:13:33 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,4 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update(key = value)
     for i, v in a_dictionary.items():
-        print("{:s}: {}".format(i, v))
+        print("{}: {}".format(i, v))
