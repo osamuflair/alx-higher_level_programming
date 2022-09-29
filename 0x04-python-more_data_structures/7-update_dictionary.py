e607a35cab390e6afb57b872a3ffb6287f01cfa8
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-09-29 07:30:12 +0000

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    #for i, v in a_dictionary.items():
-        #print("{}: {}".format(i, v))
     return a_dictionary
