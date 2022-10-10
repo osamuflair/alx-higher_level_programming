8ce9d5ab0e86820501411172b30bc11e573ce684
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-10-10 14:28:28 +0000

    python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[m], end="")
         print()
         return x
-    except (IndexError)
+    except (IndexError):
         print()
         return a
