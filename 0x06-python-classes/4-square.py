5462be7b4be1a741ec8e2214f741c2a49c354d6f
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-10-11 22:23:20 +0000

    python

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A python file to demonstrate class and object
+"""
+
+
+class Square:
+    """
+    A python class that has a value
+    """
+    def __init__(self, __size=0):
+        self.set_x(__size)
+
+    def set_x(self, __size)
+        if type(__size) != int:
+            raise TypeError("size must be an integer")
+        self.__size = __size
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def get_x(self):
+        return self.__size
+
+    def area(self):
+        return (self.__size)**2
