5716b2da29f20c7dd60d94e60fc039606ac9a41c
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-10-11 21:06:11 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A python file to demonstrate class and object
+"""
+
+class Square:
+    """
+    A python class that has a value
+    """
+    def __init__(self, __size):
+        self.__size = __size
